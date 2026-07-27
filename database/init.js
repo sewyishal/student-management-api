ddffc60229a41758ead14d497ea45886c9f14f7a
sewyishal <sewyishalx@gmail.com>
2026-07-27 21:28:50 +0300

    Added separate file for table creation

---
+++ database/init.js
@@ -0,0 +1,23 @@
+const createTables=()=>{
+    connection.query(`CREATE TABLE IF NOT EXISTS departments(
+            department_id INT AUTO_INCREMENT PRIMARY KEY ,department_name VARCHAR(255) NOT NULL )`,
+            (err,results)=>{
+                if(err){
+                    console.log(err)
+                }else{
+                    connection.query(`CREATE TABLE IF NOT EXISTS students(
+                        student_id INT AUTO_INCREMENT PRIMARY KEY ,student_name VARCHAR(255) NOT NULL, 
+                        email VARCHAR(255) UNIQUE ,department_id  INT,
+                         FOREIGN KEY(department_id) REFERENCES departments(department_id))`,
+                         (err,results)=>{
+                            if(err){
+                                console.log(err)
+                            }else{
+                                console.log("Table created successfully")
+                            }
+                         })
+                }
+            })
+} 
+
+module.exports=createTables
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const connection =require('./config/db')
+const createTables =require('./database/init')
 const departmentRoutes= require('./routes/departmentRoutes')
 const studentRoutes =require('./routes/studentRoutes')
+
 const app= express();
 
 app.use(express.json())
@@ -14,33 +16,9 @@ connection.connect((err)=>{
     if(err){
         console.log(err)
     } else{
-        console.log("Database connected successfully")
-        connection.query(`CREATE TABLE IF NOT EXISTS departments(
-            department_id INT AUTO_INCREMENT PRIMARY KEY ,department_name VARCHAR(255) NOT NULL )`,
-            (err,results)=>{
-                if(err){
-                    console.log(err)
-                }else{
-                    connection.query(`CREATE TABLE IF NOT EXISTS students(
-                        student_id INT AUTO_INCREMENT PRIMARY KEY ,student_name VARCHAR(255) NOT NULL, 
-                        email VARCHAR(255) UNIQUE ,department_id  INT,
-                         FOREIGN KEY(department_id) REFERENCES departments(department_id))`,
-                         (err,results)=>{
-                            if(err){
-                                console.log(err)
-                            }else{
-                                console.log("Table created successfully")
-                            }
-                         })
-                }
-            })
+        console.log("Database connected successfully")   
     }
 })
-
-
-
-
-
 app.listen(3000,()=>{
     console.log("App is running at http://localhost:3000/ ")
 })
