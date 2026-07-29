1f62a2c100289f78ef28c8376b240e95eec7ab84
sewyishal <sewyishalx@gmail.com>
2026-07-29 16:10:42 +0300

    Added email validation

--- middleware/validateStudent.js
+++ middleware/validateStudent.js
@@ -1,14 +1,28 @@
-const validateStudent =(req,res,next)=>{
-    if(!req.body.student_name){
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const validateStudent = (req, res, next) => {
+    if (!req.body.student_name) {
         return res.status(400).json({
             success: false,
-            message:"Student name field empty"
-        })
-    }else{
-         next()
+            message: "Student name field is empty"
+        });
     }
 
-   
-}
+    if (!req.body.email) {
+        return res.status(400).json({
+            success: false,
+            message: "Email field is empty"
+        });
+    }
 
-module.exports =validateStudent
+    if (!emailPattern.test(req.body.email)) {
+        return res.status(400).json({
+            success: false,
+            message: "Email format is not correct"
+        });
+    }
+
+    next();
+};
+
+module.exports = validateStudent;
