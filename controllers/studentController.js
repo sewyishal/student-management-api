ef902fc80f9df85e7f73421d5938d3ed1a522486
sewyishal <sewyishalx@gmail.com>
2026-08-07 03:04:17 +0300

    Changed update students in to async awaut

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -42,43 +42,35 @@ const getStudentById= async (req,res,next)=>{
         return next(err)
     }
 }
+const getStudentsWithDepartment = async (req,res,next)=>{
+    try{
+        const [results]= await connection.promise().query(`SELECT students.student_name , students.email, departments.department_name FROM students JOIN departments ON students.department_id=departments.department_id`)
 
-
-const getStudentsWithDepartment= (req,res,next)=>{ connection.query(`SELECT
-         students.student_id ,
-         students.student_name ,
-         students.email ,
-         departments.department_name
-         FROM students
-         JOIN departments
-         ON students.department_id=departments.department_id`,(err,results)=>{
-            if(err){
-                next(err)
-            } else{
-                res.json(results)
-            }
-         })
+        return res.status(200).json({
+            success: true,
+            data: results
+        })
+    } catch(err){
+        return next(err)
+    }
 }
-const updateStudent = (req,res,next)=>{
-    const sql=`UPDATE students SET 
+
+
+const updateStudent = async (req,res,next)=>{
+   try{
+     const sql=`UPDATE students SET 
     student_name=?, email=?,department_id=? WHERE student_id=?` 
     const values=[req.body.student_name,req.body.email,req.body.department_id,req.params.id]
-    connection.query(sql,values,(err,results)=>{
-        if(err){
-            next(err)
-        } else if(results.affectedRows==0){
-            return res.status(400).json({
-                success: false,
-                message:"Student not found"
-            })
-        } else{
-            console.log("Table Updates successfully")
-           return res.status(200).json({
-            success: true,
-            message:"Student updated successfully"
-           })
-        }
+    const [results]= await connection.promise().query(sql,values)
+
+    return res.status(200).json({
+        success: true,
+        message:"Student updated successfully"
     })
+   } 
+   catch(err){
+    return next(err)
+   }
 }
 
 const deleteStudent = (req,res,next)=>{
