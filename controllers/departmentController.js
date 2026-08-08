4616052d959549f077863285189af193aa36e50b
sewyishal <sewyishalx@gmail.com>
2026-08-08 12:23:04 +0300

    Added non existing id catching method to deleteDeparment

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -54,7 +54,7 @@ const updateDepartment= async (req,res,next)=>{
     try{
     const sql=`UPDATE departments SET department_name=? WHERE department_id=?`
     const values=[req.body.department_name,req.params.id]
-    const [results]= await connection.promise().query(sql,values,)
+    const [results]= await connection.promise().query(sql,values)
         
     if(results.affectedRows===0){
         return res.status(404).json({
@@ -78,6 +78,12 @@ const deleteDepartment = async (req,res,next)=>{
          const sql= `DELETE FROM departments WHERE department_id=?`
          const values=[req.params.id]
          const [results]= await connection.promise().query(sql,values)
+         if (results.affectedRows === 0) {
+          return res.status(404).json({
+           success: false,
+           message: "Department not found"
+         });
+        }
          return res.status(200).json({
             success: true,
             message: "Department deleted successfully"
