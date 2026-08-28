531a823d0a9c109d4cf9868acb4132c89d9e4f69
sewyishal <sewyishalx@gmail.com>
2026-08-29 02:13:56 +0300

    now the routes are protected with auth middleware

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,12 +2,12 @@ const express = require('express')
 const {addStudent,getAllStudents,getStudentById,getStudentsWithDepartment, updateStudent, deleteStudent} = require('../controllers/studentController');
 const validateStudent = require('../middleware/validateStudent');
 const checkDepartment = require('../middleware/checkDepartment');
-
+const authMiddleware = require('../middleware/authMiddleware')
 const router =express.Router();
 
 router.post('/students',validateStudent,checkDepartment,addStudent)
 
-router.get('/students',getAllStudents)
+router.get('/students',authMiddleware,getAllStudents)
 
 router.get('/students/:id',getStudentById)
 
