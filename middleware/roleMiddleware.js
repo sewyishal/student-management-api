59bf7269c7bd4ffd5c57497a5df99ef2864b2adb
sewyishal <sewyishalx@gmail.com>
2026-08-29 14:38:26 +0300

    added role based middleware

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,13 @@
+const roleMiddleware = (role)=>{
+    return (req,res,next)=>{
+        if(req.user.role!==role){
+            return res.status(403).json({
+                success: false,
+                message:"Access denied"
+            })
+        }
+    next()
+    }
+  
+}
+module.exports = roleMiddleware
