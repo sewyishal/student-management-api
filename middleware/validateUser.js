06f481e30cd019562f14f4c0c4c8549297139ddc
sewyishal <sewyishalx@gmail.com>
2026-08-30 23:32:43 +0300

    refactored user validator into array structure

--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,29 +1,34 @@
-const {body,validationResult}= require('express-validator')
-body("username")
-.notEmpty()
-.withMessage("Username is required")
+const { body, validationResult } = require("express-validator");
 
- body("email")
-.notEmpty()
-.withMessage("Email is required")
-.isEmail()
-.withMessage("Email format is invalid")
+const validateUser = [
+    body("username")
+        .notEmpty()
+        .withMessage("Username is required"),
 
-body("password")
-.notEmpty()
-.withMessage("password is required")
-.isLength({min: 6})
-.withMessage("Password must be at least 6 characters long")
+    body("email")
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email format is invalid"),
 
-const validateUser = (req,res,next) =>{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            success: false,
-            errors: errors.array()
-        })
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 6 })
+        .withMessage("Password must be at least 6 characters long"),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+
+        next();
     }
-    next()
-}
+];
 
-module.exports= validateUser;
+module.exports = validateUser;
