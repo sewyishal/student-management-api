b9ed2d7b9b75a3aa220ea60f7c07c8c13289efe0
sewyishal <sewyishalx@gmail.com>
2026-09-03 16:33:15 +0300

    add user registration test to User API

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,23 @@
 const request = require("supertest")
 const app = require("../app")
 
+describe("User API",()=>{
+test("should register a new user", async() =>{
+    const email = `test${Date.now()}@gmail.com`;
+    const response = await request(app)
+    .post('/register')
+    .send({
+    username: "Abebe",
+    email,
+    password: "123456"
+})
+expect(response.status).toBe(201)
+expect(response.success).toBe(true)
+expect(response.body.user_id).toBeDefined()
+})
+
+})
+
 test("Login user", async()=>{
    const response= await request(app)
     .post('/login')
