d4fdc1d79427dfaf070c46c79ca95c609dd99028
sewyishal <sewyishalx@gmail.com>
2026-09-03 18:53:40 +0300

    test: enhance user registration tests and add token validation check

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,15 +1,35 @@
 const request = require("supertest")
 const app = require("../app");
-const expectCookies = require("supertest/lib/cookies");
 
 describe("User API",()=>{
-test("should register a new user", async() =>{
-    const email = `test${Date.now()}@gmail.com`;
-    const response = await request(app)
+    let token
+    let email
+
+    beforeAll(async()=>{
+        email = `test${Date.now()}@gmail.com`;
+         await request(app)
     .post('/register')
     .send({
     username: "Abebe",
     email,
+    password: "123456"
+})
+     const loginResponse = await request(app)
+            .post("/login")
+            .send({
+                email,
+                password: "123456"
+            });
+
+        token = loginResponse.body.token;
+    })
+test("should register a new user", async() =>{
+    const newEmail = `test${Date.now()}@gmail.com`;
+    const response = await request(app)
+    .post('/register')
+    .send({
+    username: "Abebe",
+    newEmail,
     password: "123456"
 })
 expect(response.status).toBe(201)
@@ -25,13 +45,13 @@ test("should fail if email is missing", async() =>{
             })
     expect(response.status).toBe(400)
     expect(response.body.success).toBe(false)
-    expect(response.body.erros).toBeDefined()
+    expect(response.body.errors).toBeDefined()
 })
 test("should fail for duplicate email", async()=>{
     const response=await request(app).post('/register').send({
-    "username": "Abebe",
-    "email": "existing@gmail.com",
-    "password": "123456"
+    username: "Abebe",
+    email,
+    password: "123456"
     })
     expect(response.status).toBe(409)
    expect(response.body.message).toBe("Email already exists");
@@ -41,6 +61,13 @@ test("should return 401 if no token is provided",async()=>{
     expect(response.status).toBe(401)
     expect(response.body.success).toBe(false)
     expect(response.body.message).toBe("Access denied. No token provided");
+})
+test("should check if the user sent a token",async()=>{
+    const response =await request(app).get('/students').set('Authorization',`Bearer ${token}`)
+    expect(response.status).toBe(200)
+    expect(response.body.success).toBe(true);
+    expect(response.body.data).toBeDefined();
+    expect(Array.isArray(response.body.data)).toBe(true);
 })
 })
 
